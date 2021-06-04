9a31ef055ff60f36c2466b779e75cbb3b140cedc
Marcus <81660172+Marcus-Repository@users.noreply.github.com>
2021-06-04 13:45:59 -0600

    Complete rework of calculations.py

--- randomnwn/calculations.py
+++ randomnwn/calculations.py
@@ -6,198 +6,111 @@
 # Author: Marcus Kasdorf
 # Date:   May 28, 2021
 
-from matplotlib.pyplot import sci
 import numpy as np
 import scipy
 import networkx as nx
 from networkx.linalg import laplacian_matrix
 
 
-def _capacitance_matrix_JDA(NWN: nx.Graph, drain_node: tuple):
+def create_matrix(
+    NWN: nx.Graph,
+    type: str = "conductance",
+    drain_node: tuple = None,
+    ground_nodes: bool = False
+) -> scipy.sparse.csr_matrix: 
     """
-    Create the (sparse) conductance matrix for a given JDA NWN.
+    Create the Laplacian connectivity matrix.
 
-    """
-    # Get Laplacian matrix
-    wire_num = NWN.graph["wire_num"]
-    nodelist = [(i,) for i in range(wire_num)]
-    M_C = laplacian_matrix(NWN, nodelist=nodelist, weight="capacitance")
+    Parameters
+    ----------
+    NWN : Graph
+        Nanowire network.
 
-    # Ground every node with a tiny capacitor 
-    # to ensure no singular matrices: 1e-8 uF
-    M_C += scipy.sparse.dia_matrix(
-        (np.ones(wire_num) * 1e-8, [0]), shape=(wire_num, wire_num)
-    )
+    type : {"conductance", "capacitance"}, optional
+        Weight to use for the Laplacian matrix.
 
-    # Zero the drain node row
-    M_C = M_C.tolil()
-    M_C[drain_node] = 0
-    M_C[drain_node, drain_node] = 1
+    drain_node : tuple, optional
+        If a drain node is supplied, the row and column corresponding to 
+        the drain node are zeros and a one is placed at the row-column
+        intersection. Default: None.
 
-    return M_C
+    ground_nodes : bool, optional
+        If true, a small value is added to the main diagonal to avoid
+        singular matrices.
 
-
-def _capacitance_matrix_MNR(NWN: nx.Graph, drain_node: tuple):
-    """
-    Create the (sparse) conductance matrix for a given MNR NWN.
-
-    """
-    # Get Laplacian matrix
-    nodelist = sorted(NWN.nodes())
-    nodelist_len = len(nodelist)
-    M_C = laplacian_matrix(NWN, nodelist=nodelist, weight="capacitance")
-
-    # Ground every node with a tiny capacitor 
-    # to ensure no singular matrices: 1e-8 uF
-    M_C += scipy.sparse.dia_matrix(
-        (np.ones(nodelist_len) * 1e-8, [0]), shape=(nodelist_len, nodelist_len)
-    )
-
-    # Zero the drain node row
-    M_C = M_C.tolil()
-    drain_node_index = nodelist.index(drain_node)
-    M_C[drain_node_index] = 0
-    M_C[drain_node_index, drain_node_index] = 1
-
-    return M_C
-
-
-def capacitance_matrix(NWN: nx.Graph, drain_node: tuple):
-    """
-    Create the capacitance matrix for a given NWN with a specified drain node.
+    Returns
+    -------
+    M : csr_matrix
+        Resultant sparse Laplacian matrix
 
     """
-    if NWN.graph["type"] == "JDA":
-        return _capacitance_matrix_JDA(NWN, drain_node[0])
-    elif NWN.graph["type"] == "MNR":
-        return _capacitance_matrix_MNR(NWN, drain_node)
-    else:
-        raise ValueError("Nanowire network has invalid type.")
+    TYPES = ["conductance", "capacitance"]
+    if type not in TYPES:
+        raise ValueError("Invalid type.")
 
+    if type == "conductance":
+        type = "is_shorted"
 
-def _conductance_matrix_JDA(NWN: nx.Graph, drain_node: int, ground_nodes: bool):
-    """
-    Create the (sparse) conductance matrix for a given JDA NWN.
-
-    """
-    # Get Laplacian matrix
-    wire_num = NWN.graph["wire_num"]
-    nodelist = [(i,) for i in range(wire_num)]
-    G = laplacian_matrix(NWN, nodelist=nodelist, weight="is_shorted")
-
-    # Ground every node with a huge resistor 
-    # to ensure no singular matrices: 1e-8 S -> 100 MΩ
-    if ground_nodes:
-        G += scipy.sparse.dia_matrix(
-            (np.ones(wire_num) * 1e-8, [0]), shape=(wire_num, wire_num)
-        )
-
-    # Zero the drain node row
-    G = G.tolil()
-    G[drain_node] = 0
-    G[:, drain_node] = 0
-    G[drain_node, drain_node] = 1
-
-    return G
-
-
-def _conductance_matrix_MNR(NWN: nx.Graph, drain_node: tuple, ground_nodes: bool):
-    """
-    Create the (sparse) conductance matrix for a given MNR NWN.
-
-    """
     # Get Laplacian matrix
     nodelist = sorted(NWN.nodes())
     nodelist_len = len(nodelist)
-    G = laplacian_matrix(NWN, nodelist=nodelist, weight="is_shorted")
+    M = laplacian_matrix(NWN, nodelist=nodelist, weight=type)
 
-    # Ground every node with a huge resistor 
-    # to ensure no singular matrices: 1e-8 S -> 100 MΩ
+    # Ground every node with a huge resistor/tiny capacitor.
     if ground_nodes:
-        G += scipy.sparse.dia_matrix(
+        M += scipy.sparse.dia_matrix(
             (np.ones(nodelist_len) * 1e-8, [0]), shape=(nodelist_len, nodelist_len)
         )
 
     # Zero the drain node row
-    G = G.tolil()
-    drain_node_index = nodelist.index(drain_node)
-    G[drain_node_index] = 0
-    G[:, drain_node_index] = 0
-    G[drain_node_index, drain_node_index] = 1
+    if drain_node is not None:
+        M = M.tolil()
+        drain_index = nodelist.index(drain_node)
+        M[drain_index] = 0
+        M[:, drain_index] = 0
+        M[drain_index, drain_index] = 1
 
-    return G
+    return M.tocsr()
 
 
-def conductance_matrix(NWN: nx.Graph, drain_node: tuple, ground_nodes: bool = True):
+def _solver(A, z, solver):
     """
-    Create the conductance matrix for a given NWN with a specified drain node.
+    Solve sparse matrix equation.
 
     """
-    if NWN.graph["type"] == "JDA":
-        return _conductance_matrix_JDA(NWN, drain_node[0], ground_nodes)
-    elif NWN.graph["type"] == "MNR":
-        return _conductance_matrix_MNR(NWN, drain_node, ground_nodes)
+    x = None
+
+    if solver == "spsolve":
+        x = scipy.sparse.linalg.spsolve(A.tocsr(), z)
+    elif solver == "minres":
+        x, exit_code = scipy.sparse.linalg.minres(A, z.toarray())
+    elif solver == "lgmres":
+        x, exit_code = scipy.sparse.linalg.lgmres(A, z.toarray())
     else:
-        raise ValueError("Nanowire network has invalid type.")
+        raise ValueError("Invalid solver.")
+
+    return x
 
 
-def solve_network(
+def _solve_voltage(
     NWN: nx.Graph, 
+    voltage: float, 
     source_node: tuple, 
-    drain_node: tuple, 
-    voltage: float,
-    solver: str = "spsolve",
-    verbose: bool = False
+    drain_node: tuple,
+    solver: str
 ) -> np.ndarray:
     """
-    Solve for the voltages of each wire in a given NWN.
-    The source node will be at the specified voltage and
-    the drain node will be grounded.
-    
-    """
-    if solver not in ["spsolve", "minres", "lgmres"]:
-        raise ValueError("Solver is not a valid argument.")
+    Solve for voltages at all the nodes for a given supplied voltage.
 
+    """
     # Find node ordering and indexes
     nodelist = sorted(NWN.nodes())
     nodelist_len = len(nodelist)
     source_index = nodelist.index(source_node)
-    drain_index = nodelist.index(drain_node)
 
-    ground_nodes = False if solver == "minres" else True
-    ground_nodes = True
+    ground_nodes = True if solver == "spsolve" else False
 
-    # Calculate junction capacitances to determine shorted junctions
-    M_C = capacitance_matrix(NWN, drain_node)
-
-    # Create block matrix to solve network for voltage and charge
-    B = scipy.sparse.dok_matrix((nodelist_len, 1))
-    B[source_index, 0] = -1
-
-    C = -B.T
-
-    D = None
-
-    A = scipy.sparse.bmat([[M_C, B], [C, D]])
-    z = scipy.sparse.dok_matrix((nodelist_len + 1, 1))
-    z[-1] = voltage
-
-    # Solve linear equations
-    *voltage_list, charge = scipy.sparse.linalg.spsolve(A.tocsr(), z)
-
-
-    # Stored activation data in edges
-    for node1, node2 in NWN.edges():
-        node1_ind = nodelist.index(node1)
-        node2_ind = nodelist.index(node2)
-        voltage_drop = abs(voltage_list[node1_ind] - voltage_list[node2_ind])
-        
-        if voltage_drop < NWN.graph["break_voltage"]:
-            NWN.edges[(node1, node2)]["is_shorted"] = 0
-
-
-    # Solve the network only for the shorted junctions
-    G = -conductance_matrix(NWN, drain_node, ground_nodes)
+    G = -create_matrix(NWN, "conductance", drain_node, ground_nodes)
     B = scipy.sparse.dok_matrix((nodelist_len, 1)); B[source_index, 0] = 1
     C = B.T
     D = None
@@ -206,15 +119,117 @@ def solve_network(
     z = scipy.sparse.dok_matrix((nodelist_len + 1, 1))
     z[-1] = voltage
 
-    # SparseEfficiencyWarning: spsolve requires A be CSC or CSR matrix format
-    if solver == "spsolve":
-        x = scipy.sparse.linalg.spsolve(A.tocsr(), z)
-        return x
-    elif solver == "minres":
-        x, exit_code = scipy.sparse.linalg.minres(A, z.toarray())
-        return x
-    elif solver == "lgmres":
-        x, exit_code = scipy.sparse.linalg.lgmres(A, z.toarray())
-        return x
+    out = _solver(A, z, solver)
+    return np.array(out, copy=False)
+
+
+def _solve_current(    
+    NWN: nx.Graph, 
+    current: float, 
+    source_node: tuple, 
+    drain_node: tuple,
+    solver: str
+) -> np.ndarray:
+    """
+    Solve for voltages at all the nodes for a given supplied current.
+
+    """
+    # Find node ordering and indexes
+    nodelist = sorted(NWN.nodes())
+    nodelist_len = len(nodelist)
+    source_index = nodelist.index(source_node)
+
+    ground_nodes = True if solver == "spsolve" else False
+
+    G = create_matrix(NWN, "conductance", drain_node, ground_nodes)
+    z = scipy.sparse.dok_matrix((nodelist_len, 1))
+    z[source_index] = current
+
+    out = _solver(G, z, solver)
+    return np.array(out, copy=False)
+
+
+def solve_network(
+    NWN: nx.Graph, 
+    source_node: tuple, 
+    drain_node: tuple, 
+    input: float,
+    type: str = "voltage",
+    solver: str = "spsolve"
+) -> np.ndarray:
+    """
+    Solve for the voltages of each node in a given NWN. 
+    The drain node will be grounded. If the type is voltage, 
+    the source node will be the input voltage.
+
+    Parameters
+    ----------
+    NWN : Graph
+        Nanowire network.
+
+    source_node : tuple
+        Voltage/current source node.
+
+    drain_node : tuple
+        Grounded output node.
+
+    input : float
+        Supplied voltage/current.
+
+    type : {"voltage", "current"}, optional
+        Input type. Default: "voltage".
+
+    solver: str, optional
+        Name of sparse matrix solving algorithm to use.
+        Default: "spsolve".
+
+    Returns
+    -------
+    out : ndarray
+        Output array containing the voltages of each node. If the input type
+        is voltage, the current is also in this array.
+        
+    """
+
+    # # Calculate junction capacitances to determine shorted junctions
+    # M_C = capacitance_matrix(NWN, drain_node)
+
+    # # Create block matrix to solve network for voltage and charge
+    # B = scipy.sparse.dok_matrix((nodelist_len, 1))
+    # B[source_index, 0] = -1
+
+    # C = -B.T
+
+    # D = None
+
+    # A = scipy.sparse.bmat([[M_C, B], [C, D]])
+    # z = scipy.sparse.dok_matrix((nodelist_len + 1, 1))
+    # z[-1] = voltage
+
+    # # Solve linear equations
+    # *voltage_list, charge = scipy.sparse.linalg.spsolve(A.tocsr(), z)
+
+
+    # # Stored activation data in edges
+    # for node1, node2 in NWN.edges():
+    #     node1_ind = nodelist.index(node1)
+    #     node2_ind = nodelist.index(node2)
+    #     voltage_drop = abs(voltage_list[node1_ind] - voltage_list[node2_ind])
+        
+    #     if voltage_drop < NWN.graph["break_voltage"]:
+    #         NWN.edges[(node1, node2)]["is_shorted"] = 0
+
+
+    # Solve the network
+    out = None
+
+    if type == "voltage":
+        out = _solve_voltage(NWN, input, source_node, drain_node, solver)
+    elif type == "current":
+        out = _solve_current(NWN, input, source_node, drain_node, solver)
+    else:
+        raise ValueError("Invalid type.")
+
+    return out
 
 
