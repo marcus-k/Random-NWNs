4c7d9adbf1085c90aaa6078feb0e423fded6332b
marcus-k <81660172+marcus-k@users.noreply.github.com>
2024-05-02 19:11:52 -0600

    Add install requires to setup

--- setup.py
+++ setup.py
@@ -8,15 +8,22 @@ with open("README.md", "r", encoding="utf-8") as f:
     long_description = f.read()
 
 setup(
-    name='randomnwn',
+    name="randomnwn",
     version=namespace["__version__"],    
-    description='Modelling and analyzing random nanowire networks in Python.',
+    description="Modelling and analyzing random nanowire networks in Python.",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url='https://github.com/marcus-k/Random-NWNs',
-    author='Marcus Kasdorf',
-    author_email='marcus.kasdorf@ucalgary.ca',
-    license='MIT License',
-    packages=['randomnwn'],
-    python_requires='>=3.10'
+    url="https://github.com/marcus-k/Random-NWNs",
+    author="Marcus Kasdorf",
+    author_email="marcus.kasdorf@ucalgary.ca",
+    license="MIT License",
+    packages=["randomnwn"],
+    python_requires=">=3.10",
+    install_requires=[
+        "matplotlib>=3.8.4",
+        "networkx>=3.1",
+        "numpy>=1.26.4",
+        "scipy>=1.13.0",
+        "Shapely>=2.0.4,<3",
+    ],
 )
